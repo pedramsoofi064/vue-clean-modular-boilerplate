eb2569aa678da1742eb025a8211d692252994181
Pedram <pedramsoofi064@gmail.com>
2025-12-06 12:46:47 +0200

    chore: update git ignore

--- node_modules/@vue/apollo-composable/dist/util/paramToReactive.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Ref } from 'vue-demi';
-import type { ReactiveFunction } from './ReactiveFunction';
-type TObject = object;
-export declare function paramToReactive<T extends TObject>(param: T | Ref<T> | ReactiveFunction<T>): T | Ref<T>;
-export {};
--- node_modules/@vue/apollo-composable/dist/util/toApolloError.d.ts
+++
@@ -1,4 +0,0 @@
-import type { GraphQLFormattedError } from 'graphql';
-import { ApolloError } from '@apollo/client/core/index.js';
-export declare function toApolloError(error: unknown): ApolloError;
-export declare function resultErrorsToApolloError(errors: ReadonlyArray<GraphQLFormattedError>): ApolloError;
